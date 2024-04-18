be8375e0773ac596768c905a4cbac7f86e8607be
tdcinterns <tdcinterns@thedevcorporate.com>
2024-04-18 17:39:20 +0500

    handling GET comments request in route-handler

---
+++ route-handler-demo/src/app/comments/data.js
@@ -0,0 +1,14 @@
+export const comments = [
+    {
+        id: 1,
+        text: "This is my first comment",
+    },
+    {
+        id: 2,
+        text: "This is my second comment",
+    },
+    {
+        id: 3,
+        text: "This is my third comment",
+    },
+];
