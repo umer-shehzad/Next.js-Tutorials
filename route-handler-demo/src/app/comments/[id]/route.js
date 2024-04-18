ceacf31cfa7430c58fed9d72b6e19acd8d95740c
tdcinterns <tdcinterns@thedevcorporate.com>
2024-04-18 18:04:33 +0500

    handling PATCH update-comment request in route-handler

--- route-handler-demo/src/app/comments/[id]/route.js
+++ route-handler-demo/src/app/comments/[id]/route.js
@@ -6,3 +6,13 @@ export async function GET(request, { params }) {
     );
     return Response.json(comment);
 }
+
+export async function PATCH(request, { params }) {
+    const body = await request.json();
+    const { text } = body;
+    const index = comments.findIndex(
+        (comment) => comment.id === parseInt(params.id)
+    );
+    comments[index].text = text;
+    return Response.json(comments[index]);
+}
