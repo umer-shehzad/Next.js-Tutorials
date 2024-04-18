74bad40495fd0f5b457a58c5a6afc65465646218
tdcinterns <tdcinterns@thedevcorporate.com>
2024-04-18 18:31:28 +0500

    search/filter comments using URL query parameter in route-handler

--- route-handler-demo/src/app/comments/route.js
+++ route-handler-demo/src/app/comments/route.js
@@ -1,7 +1,12 @@
 import { comments } from "./data";
 
-export async function GET() {
-    return Response.json(comments);
+export async function GET(request) {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get("query");
+    const filteredComments = query 
+    ? comments.filter(comment => comment.text.includes(query))
+    : comments;
+    return Response.json(filteredComments);
 }
 
 export async function POST(request) {
